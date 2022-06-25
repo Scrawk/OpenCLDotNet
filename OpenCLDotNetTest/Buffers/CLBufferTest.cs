2aec2afa2bdb49bc32471f778681ca034852b43c
Justin <29545213+Scrawk@users.noreply.github.com>
2022-06-25 08:15:06 +0800

    Update CLBufferTest.cs

--- OpenCLDotNetTest/Buffers/CLBufferTest.cs
+++ OpenCLDotNetTest/Buffers/CLBufferTest.cs
@@ -1,9 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using OpenCLDotNet.Core;
+using OpenCLDotNet.Buffers;
+using OpenCLDotNet.Events;
+using OpenCLDotNet.Utility;
+using OpenCLDotNet.Programs;
 
 namespace OpenCLDotNetTest.Buffers
 {
+    [TestClass]
     public class CLBufferTest
     {
+        [TestMethod]
+        public void ConstructorTest
+        {
+
+        }
+
+        [TestMethod]
+        public void CreateReadBufferTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void Create WriteBufferTest()
+        {
+
+        }
+
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+        
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+
     }
 }
